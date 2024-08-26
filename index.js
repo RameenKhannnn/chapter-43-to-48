e500244e1e14205096c67e0e3089192359513eea
RameenKhannnn <rameenkhan8787@gmail.com>
2024-08-26 17:40:34 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+//  JavaScript is a high-level, interpreted programming language commonly used 
+//  to create interactive effects within web browsers. It is a core technology of the World Wide Web, 
+//  alongside HTML and CSS, and enables the development of dynamic web pages by allowing developers to implement 
+//  complex features like animations, form validation, and interactive content.
+ 
+
